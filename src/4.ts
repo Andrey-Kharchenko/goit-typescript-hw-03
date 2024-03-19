32dc23daccde5ae228b90bd255c4111dca21bba3
Andrey-Kharchenko <gavryk14@gmail.com>
2024-03-19 19:16:16 +0200

    Update

--- src/4.ts
+++ src/4.ts
@@ -1,4 +1,70 @@
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  public door: boolean = false;
+  public key: Key;
+  private tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log("Enter the house.");
+    } else {
+      console.log("The door is closed.");
+    }
+  }
+
+  abstract openDoor(key: Key): void;
+
+  setDoorStatus(status: boolean): void {
+    this.door = status;
+  }
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super(key);
+  }
+
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.setDoorStatus(true);
+      console.log("The door is opened.");
+    } else {
+      console.log("Invalid key. The door remains closed.");
+    }
+  }
+}
+
+
 const key = new Key();
+console.log(key.getSignature())
 
 const house = new MyHouse(key);
 const person = new Person(key);
@@ -6,6 +72,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
